c18516edd4f2f2fe3b3a7f8d5a6ead7fc2a94a80
Leo Rossignac-Milon <leo.milon@gmail.com>
2014-08-23 01:41:40 -0400

    Fix Warning for IncorporationError

--- src/contiguityTree/IncorporationError.java
+++ src/contiguityTree/IncorporationError.java
@@ -1,5 +1,7 @@
 package contiguityTree;
 
 public class IncorporationError extends Exception {
+    private static final long serialVersionUID = 1L;
+
     public IncorporationError(String message) { super(message); }
 }
